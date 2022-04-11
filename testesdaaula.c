09e15dd17c4b63e45e8494b1cad36dc1bb52a03d
JonasFOliveira <jff.oliveira2018@gmail.com>
2022-04-11 18:40:04 -0300

    Update

--- testesdaaula.c
+++ testesdaaula.c
@@ -5,6 +5,7 @@ int alfabeto(){
        printf("%c", i);
     }
 }
+
 int caracteres(){
     char caracter;
     int i = 0;
@@ -18,8 +19,116 @@ int caracteres(){
     printf("%d\n", i);
 }
 
+int decrescente(){
+
+    int a[10];
+
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = 9; i >= 0; i--){
+        printf("%d", a[i]);
+        if (i >0)
+        printf(", ");
+    }
+}
+
+int frequencia(){
+    int a[10];
+    int num[10];
+
+    for(int i = 0; i < 10; i++){
+        num[i] = 0;
+    }
+
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &a[i]);
+
+        switch (a[i])
+        {
+        case 0:
+            num[0]++;
+            break;
+        case 1:
+            num[1]++;
+            break;
+        case 2:
+            num[2]++;
+            break;
+        case 3:
+            num[3]++;
+            break;
+        case 4:
+            num[4]++;
+            break;
+        case 5:
+            num[5]++;
+            break;
+        case 6:
+            num[6]++;
+            break;
+        case 7:
+            num[7]++;
+            break;
+        case 8:
+            num[8]++;
+            break;
+        case 9:
+            num[9]++;
+            break;
+        }
+    }
+
+     for(int i = 0; i < 10; i++){
+        printf("%d : %d\n", i, num[i]);
+    }
+
+}
+
+int frequencia02(){
+    int a[10];
+    int num[10];
+
+    for(int i = 0; i < 10; i++){
+        num[i] = 0;
+    }
+
+    for (int i = 0; i < 10; i++){
+        scanf("%d", &a[i]);
+
+        for (int j = 0; j < 10; j++){
+            if (j == a[i]){
+                num[j]++;
+            }
+        }
+    }
+    
+    for(int i = 0; i < 10; i++){
+        printf("%d : %d\n", i, num[i]);
+    }
+}
+
+int frequenciaProf(){
+    int a;
+    int num[10];
+
+    for(int i = 0; i < 10; i++){
+        num[i] = 0;
+    }
+
+    for (int i = 0; i < 10; i++){
+        scanf("%d", &a);
+        num[a]++;
+    }
+    
+    for(int i = 0; i < 10; i++){
+        printf("%d : %d\n", i, num[i]);
+    }
+
+}
 int main(){
 
-    caracteres();
+    frequencia02();
     return 0;
 }
